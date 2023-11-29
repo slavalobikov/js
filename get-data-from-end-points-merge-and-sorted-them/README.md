da8611200e32d7f00a92cb44ab3292f4d2c3a60c
slava.lobikov <lobikova12@mail.ru>
2023-11-29 22:14:28 +0300

    2

---
+++ get-data-from-end-points-merge-and-sorted-them/README.md
@@ -0,0 +1,5 @@
+#Write a method which will get data from the following endpoints  
+1. [https://services.odata.org/Experimental/OData/OData.svc/Products](https://services.odata.org/Experimental/OData/OData.svc/Products)  
+2. [https://services.odata.org/Experimental/OData/OData.svc/ProductDetails](https://services.odata.org/Experimental/OData/OData.svc/ProductDetails)  
+# Merge results together  
+# Sort results by price field in ascending order
---
+++ get-data-from-end-points-merge-and-sorted-them/index.js
@@ -0,0 +1,26 @@
+const productEndPoint = 'https://services.odata.org/Experimental/OData/OData.svc/Products'
+const detailsEndPoint = 'https://services.odata.org/Experimental/OData/OData.svc/ProductDetails'
+
+const productPromise = fetch(productEndPoint).then(res => res.json())
+const detailsPromise = fetch(detailsEndPoint).then(res => res.json())
+
+const ul = document.getElementById('products')
+
+Promise.all([productPromise, detailsPromise]).then(res => {
+    const [{value:products}, {value:details}] = res
+    products.sort((a,b) => a.Price - b.Price)
+
+
+    details.forEach(detail => {
+         const indexProduct = products.findIndex(product => product.ID === detail.ProductID)
+        products[indexProduct] = {...products[indexProduct] ,...detail}
+    })
+
+    products.forEach(product => {
+        const productElement = document.createElement('li')
+        productElement.innerText = `${product.Name}, ${product.Details ? `${product.Details},` : ''} ${product.Price}$`
+        ul.appendChild(productElement)
+    })
+
+    console.log('result', products)
+})
