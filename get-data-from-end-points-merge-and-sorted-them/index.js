2eadbdd68166043db2bc07c22cb6e510564fd782
slava.lobikov <lobikova12@mail.ru>
2023-12-04 11:32:20 +0300

    add async await methods

--- get-data-from-end-points-merge-and-sorted-them/index.js
+++ get-data-from-end-points-merge-and-sorted-them/index.js
@@ -1,3 +1,4 @@
+/*
 const productEndPoint = 'https://services.odata.org/Experimental/OData/OData.svc/Products'
 const detailsEndPoint = 'https://services.odata.org/Experimental/OData/OData.svc/ProductDetails'
 
@@ -24,3 +25,34 @@ Promise.all([productPromise, detailsPromise]).then(res => {
 
     console.log('result', products)
 })
+*/
+
+const productEndPoint = 'https://services.odata.org/Experimental/OData/OData.svc/Products'
+const detailsEndPoint = 'https://services.odata.org/Experimental/OData/OData.svc/ProductDetails'
+const ul = document.getElementById('products')
+
+
+const setDataWithAsync = async () => {
+    const urls = [productEndPoint, detailsEndPoint]
+    const arr = await Promise.all(urls.map(async url => {
+        const res = await fetch(url)
+        return res.json()
+    }))
+
+    const [{value:products}, {value:details}] = arr
+    products.sort((a,b) => a.Price - b.Price)
+
+
+    details.forEach(detail => {
+        const indexProduct = products.findIndex(product => product.ID === detail.ProductID)
+        products[indexProduct] = {...products[indexProduct] ,...detail}
+    })
+
+    products.forEach(product => {
+        const productElement = document.createElement('li')
+        productElement.innerText = `${product.Name}, ${product.Details ? `${product.Details},` : ''} ${product.Price}$`
+        ul.appendChild(productElement)
+    })
+}
+
+setDataWithAsync()
